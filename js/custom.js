41bed00e87b2135a6cb47682290c55b17c76cd45
katunz jr <84384453+Jasper-Rupia@users.noreply.github.com>
2022-05-16 02:25:15 +0300

    me

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,26 @@ function lowerCaseProfile() {
 
 
 
-// ---------------------------------- all pages
+// ---------------------------------- back to top
+//Get the button
+var mybutton = document.getElementById("btn-back-to-top");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function scrollFunction(){
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+};
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+
+
+ // ---------------------------------- all pages
 $(function() {
     "use strict";
     $(function() {
@@ -162,3 +181,5 @@ $(function() {
     });
 
 });
+
+
---
+++ js/register-form.js
@@ -0,0 +1,38 @@
+
+
+// ---------------------------------- form wizard (register)
+function summaryPaste() {
+    document.getElementById("userNamePaste").innerHTML = document.getElementById("userName").value;
+    var emailToLowerString = document.getElementById("email").value.toLowerCase();
+    document.getElementById("email").value = emailToLowerString;
+    document.getElementById("emailPaste").innerHTML = emailToLowerString;
+    document.getElementById("phamarcyNamePaste").innerHTML = document.getElementById("phamarcyName").value;
+    document.getElementById("locationAddressPaste").innerHTML = document.getElementById("locationAddress").value;
+}
+var form = $("#register-form");
+form.validate({
+  errorPlacement: function errorPlacement(error, element) {
+    element.before(error);
+  },
+  rules: {
+    confirm: {
+      equalTo: "#password",
+    },
+  },
+});
+form.children("div").steps({
+  headerTag: "h3",
+  bodyTag: "section",
+  transitionEffect: "slideLeft",
+  onStepChanging: function (event, currentIndex, newIndex) {
+    form.validate().settings.ignore = ":disabled,:hidden";
+    return form.valid();
+  },
+  onFinishing: function (event, currentIndex) {
+    form.validate().settings.ignore = ":disabled";
+    return form.valid();
+  },
+  onFinished: function (event, currentIndex) {
+    alert("Submitted!");
+  },
+});
