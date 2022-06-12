41c2a6fca8baaf13c6f955d33c670d8413093459
katunz jr <84384453+Jasper-Rupia@users.noreply.github.com>
2022-06-12 12:32:15 +0300

    added

--- bot/views.py
+++ bot/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, )
+    mysite = loader.get_template('index.html')
+    return HttpResponse(mysite.render())
+    #return render(request, '')
