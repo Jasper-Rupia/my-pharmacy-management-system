e5ff9217f60d4529773d87cf09aeb90fcaa073ec
katunz jr <84384453+Jasper-Rupia@users.noreply.github.com>
2022-06-17 06:32:17 +0300

    .

--- medicine/models.py
+++ medicine/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Stock(models.Model):
+    name = models.CharField(max_length=30)
+    generic_name = models.CharField(max_length=30)
+    category_name = models.ForeignKey('Category', on_delete=models.CASCADE)
+    packaging = models.IntegerField()
+    quantity = models.CharField(max_length=5)
+    cost = models.IntegerField()
+    price = models.IntegerField()
+    best_before = models.DateField()
+    status = models.CharField(max_length=12)
